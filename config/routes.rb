77cc808f852fc0a627729b9b90a180fdbd6bc30e
rostyslav kanibolotskyi <rkanibolotskyi@gmail.com>
2022-02-22 11:12:37 +0100

    home-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tas#home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tas, only: [:index, :show] do
     resources :bookings, [:new, :create]
