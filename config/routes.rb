7b601a677079942c83b2abf28377eb67678242ad
rostyslav kanibolotskyi <rkanibolotskyi@gmail.com>
2022-02-22 10:40:29 +0100

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tas#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tas, only: [:index, :show]do
+  resources :tas, only: [:index, :show] do
     resources :bookings, [:new, :create]
   end
-    resources :bookings, [:index, :show, :destroy]
+
+  resources :users do
+    resources :bookings, only: [:index]
+  end
+
+  resources :bookings, [:show, :destroy]
 end
