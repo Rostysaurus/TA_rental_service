5e002e10fc4c7c3af12acc4d973bba531693456e
Viktor Tagil <viktor.tagil@gmail.com>
2022-02-23 21:30:15 +0100

    added review link

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,26 @@
 class ReviewsController < ApplicationController
-  before_action :find_restaurant
+  before_action :find_teacher
 
   def new
     @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    # @review.user = current_user
+    @review.teacher = @teacher
+    @review.save
+    redirect_to teacher_path(@teacher)
   end
 
   private
 
-  def find_restaurant
+  def find_teacher
     @teacher = Teacher.find(params[:teacher_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment)
   end
 
 end
