c3937cc58060a6c8d8f8c148504af2398fc04e12
Viktor Tagil <viktor.tagil@gmail.com>
2022-02-23 20:43:13 +0100

    review form added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+  end
+
+  private
+
+  def find_restaurant
+    @teacher = Teacher.find(params[:teacher_id])
+  end
+
 end
