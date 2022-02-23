17ab1fb17c59e3bd47823a98e8f162a91cd6a535
Kinga Ozimek <kingaozime@gmail.com>
2022-02-23 15:11:33 +0100

    index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :teachers, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:new, :create]
+    collection do
+      get :home
+    end
   end
   resources :bookings, only: [:index, :show, :destroy]
 end
