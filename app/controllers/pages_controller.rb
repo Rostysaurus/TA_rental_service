883ea10ffae763afc98dab9927aad481b5282e64
Kinga Ozimek <kingaozime@gmail.com>
2022-02-23 02:24:50 +0100

    index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @teachers = Teacher.all
   end
 end
---
+++ app/models/ta.rb
@@ -0,0 +1,5 @@
+class Ta < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
+end
